4bbafee0950190e9751a875b9ff62467df9c9c97
josh-may6 <joshmaitre6@gmail.com>
2023-12-08 09:15:32 -0800

    Functioning GUI

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas
+from tkinter import *
+import pyperclip
 
 
 def end_loop(entry_input):
@@ -20,8 +22,8 @@ def text_to_morse(user_input):
 
     morse_code_list = [morse_dict[letter] for letter in user_input]
     morse_code_string = ' '.join(morse_code_list)
-
-    return morse_code_string
+    output_entry.insert(0, morse_code_string)
+    pyperclip.copy(morse_code_string)
 
 
 def morse_to_text(user_input):
@@ -30,24 +32,47 @@ def morse_to_text(user_input):
     morse_input_list = user_input.split(" ")
     tran_list = [morse_dict[char] for char in morse_input_list]
     trans_string = "".join(tran_list)
-    return trans_string
+    output_entry.insert(0, trans_string)
+    pyperclip.copy(trans_string)
 
 
-def translate(user_input):
+def translate():
+    user_input = input_entry.get().lower()
+    output_entry.delete(0, END)
     if is_mores_code(user_input):
         return morse_to_text(user_input)
     else:
         return text_to_morse(user_input)
 
 
-continue_translating = True
+window = Tk()
+window.title("Morse Code Translator")
+window.config(padx=50, pady=50)
 
-while continue_translating:
-    user_word = input("What would you like to translate? ").lower()
-    continue_translating = end_loop(user_word)
-    if continue_translating:
-        result = translate(user_word)
-        print(f"Translated: {result}")
-print("\nThank you for using Morse Code Translator")
+canvas = Canvas(width=200, height=200)
+telegraph_image = PhotoImage(file="key-telegraph-Morse-1860.jpg copy.png")
+canvas.create_image(100, 100, image=telegraph_image)
+canvas.grid(column=1, row=0)
 
+# Labels
+input_label = Label(text="Input")
+input_label.grid(column=0, row=1)
 
+output_label = Label(text="Output")
+output_label.grid(column=0, row=3)
+
+# Buttons
+translate_button = Button(text="Translate", width=13, command=translate)  # Add function to button for translate funtion command=translate
+translate_button.grid(column=2, row=1)
+
+add_button = Button(text="Add to Favorites", width=36)  # Add function for save responses command=save
+add_button.grid(column=1, row=4)
+
+# Entries
+input_entry = Entry(width=35)
+input_entry.grid(column=1, row=1)
+
+output_entry = Entry(width=35)
+output_entry.grid(column=1, row=3)
+
+window.mainloop()
